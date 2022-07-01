be0ea4d0e9c11bcb334a1dabe708308f4ed32064
Sowatee <jamesprescott9000@gmail.com>
2022-06-30 19:04:36 -0500

    Added functionality to search for ingredients by name or one of their effects

--- src/context.js
+++ src/context.js
@@ -6,10 +6,62 @@ const IngredientContext = React.createContext();
 const IngredientProvider = ({ children }) => {
   // alphabetically sort data
   const [ingredients, setIngredients] = useState(data);
+  const [search, setSearch] = useState('');
 
-  const filterIngredientsByClick = (effects, card) => {
+  useEffect(() => {
+    searchIngredients();
+  }, [search]);
+
+  const resetIngredients = () => {
+    console.log('button clicked');
+    setIngredients(data);
+  }
+
+  const getEffects = (card) => {
+    const effects = [];
+
+    if (card['good-effects']) {
+      for (let effect of card['good-effects']) {
+        effects.push(effect.toLowerCase());
+      }
+    }
+    if (card['bad-effects']) {
+      for (let effect of card['bad-effects']) {
+        effects.push(effect.toLowerCase());
+      }
+    }
+    return effects;
+  }
+
+  const searchIngredients = () => {
+    if (search === "") {
+      setIngredients(data);
+    }
+    const newList = data.filter((ingr) => {
+      const ingrName = ingr.name.toLowerCase();
+      const effects = getEffects(ingr);
+
+      if (ingrName.includes(search.toLowerCase())) {
+        return ingr;
+      }
+
+      for (let effect of effects) {
+        if (effect.includes(search.toLowerCase())) {
+          return ingr;
+        }
+      }
+
+    });
+    if (newList.length === 0) {
+
+    }
+    setIngredients(newList);
+  }
+
+  const filterIngredientsByClick = (card) => {
     
-    
+    const effects = getEffects(card);
+
     const newList = data.filter((item) => {
       if (item.name === card.name) return;
 
@@ -37,6 +89,8 @@ const IngredientProvider = ({ children }) => {
     <IngredientContext.Provider
       value={{
         ingredients,
+        setSearch,
+        resetIngredients,
         filterIngredientsByClick
       }}
     >
