c537947c1fc29fec18291e692a24a4c129834eec
Sowatee <jamesprescott9000@gmail.com>
2022-06-30 19:03:56 -0500

    Created a header element that contains the search button and reset button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import IngredientsList from './components/IngredientsList';
+import Header from './components/Header';
 function App() {
   return (
     <>
+      <Header/>
       <IngredientsList />
     </>
   );
